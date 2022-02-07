e6a212bf5fb5826f6e084cee875ad110f7e53e55
jcastillo501 <jcastillo501@unab.edu.co>
2022-02-07 14:22:34 -0500

    Actualizacon a las screens

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:lugarenos/screens/screenInfo/infoPlaces.dart';
+import 'package:lugarenos/screens/views/components/Apis/place.dart';
 import 'package:lugarenos/screens/views/viewMain.dart';
 
 void main() => runApp(MyApp());
